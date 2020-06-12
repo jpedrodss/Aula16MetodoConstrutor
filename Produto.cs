cb2e29abc3ec0e4441a4b4131285766e81c21cf3
João Pedro <jpedrodss@gmail.com>
2020-06-12 11:37:03 -0300

    projeto finalizado

---
+++ Produto.cs
@@ -0,0 +1,23 @@
+namespace Aula16MetodoConstrutor
+{
+    public class Produto
+    {
+        public Produto(){
+
+        }
+        public Produto(int _codigo){
+            this.Codigo = _codigo;
+        }
+        public Produto(int _codigo, string _nome, string _descricao, int _estoque){
+            this.Codigo     = _codigo;
+            this.Nome       = _nome;
+            this.Descricao  = _descricao;
+            this.Estoque    = _estoque;
+        }
+
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Estoque { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace Aula16MetodoConstrutor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Produto celular = new Produto();
+
+            Produto tenis = new Produto(588);
+            System.Console.WriteLine(tenis.Codigo);            
+            Produto carro = new Produto(549, "Peugeot", "carro vrum vrum", 1);
+            System.Console.WriteLine($"\n{carro.Codigo}\n{carro.Nome}\n{carro.Descricao}\n{carro.Estoque}");
         }
     }
 }
